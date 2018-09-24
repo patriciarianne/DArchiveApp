7d2ee7921950f2e045dc437a7c6cc6193dfcf115
patriciarianne <ariannebelits@gmail.com>
2018-09-24 16:08:18 +0800

    Added test cases for library contract

--- test/libraryTest.js
+++ test/libraryTest.js
@@ -1,5 +1,5 @@
 const Library = artifacts.require("./Library.sol")
-
+const ETHER = 10 ** 18
 contract("Library", async (accounts) => {
   let library
 
@@ -13,6 +13,11 @@ contract("Library", async (accounts) => {
       const booksCount = await library.getBookCount()
       assert.equal(booksCount, 0);
     })
+
+    it('should have zero contract balance', async () => {
+      const contractBalance = await library.getContractBalance()
+      assert.equal(contractBalance, 0);
+    })
   })
 
   describe('Main functions of the Library contract', () => {
@@ -21,7 +26,7 @@ contract("Library", async (accounts) => {
         'Test book',
         'Test description',
         'Test genre',
-        10,
+        10 * ETHER,
         'testLinkHash',
         'testImageHash',
         { from: accounts[0] }
@@ -35,7 +40,7 @@ contract("Library", async (accounts) => {
         'Test2 book',
         'Test2 description',
         'Test2 genre',
-        10,
+        10 * ETHER,
         'test2LinkHash',
         'test2ImageHash',
         { from: accounts[1] }
@@ -44,13 +49,13 @@ contract("Library", async (accounts) => {
         'Test3 book',
         'Test3 description',
         'Test3 genre',
-        10,
+        10 * ETHER,
         'test3LinkHash',
         'test3ImageHash',
         { from: accounts[1] }
       )
-      const booksCount = await library.getBookCount()
-      assert.equal(booksCount, 3);
+      const booksCount2 = await library.getBookCount()
+      assert.equal(booksCount2, 3);
     })
 
     it('should get the book by index', async () => {
@@ -59,16 +64,34 @@ contract("Library", async (accounts) => {
       assert.equal(description, 'Test description')
       assert.equal(author, accounts[0])
       assert.equal(genre, 'Test genre')
-      assert.equal(price, 10)
+      assert.equal(price, 10 * ETHER)
       assert.equal(linkHash, 'testLinkHash')
       assert.equal(imageHash, 'testImageHash')
     })
-  })
 
-  it('should remove the book by index', async () => {
-    await library.removeBook(1)
-    const booksCount = await library.getBookCount()
-      assert.equal(booksCount, 2);
-  })
+    it('should remove the book by index', async () => {
+      await library.removeBook(1, { from: accounts[1] })
+      const booksCount = await library.getBookCount()
+      assert.equal(booksCount, 2)
+      try {
+        await library.getBookAt(1)
+      } catch (error) {
+        assert.equal(error, 'Book is not available')
+      }
+    })
+  
+    it('should not remove the book when user is not the author', async () => {
+      try {
+        await library.removeBook(0, { from: accounts[1] })
+      } catch (error) {
+        assert.equal(error, 'Not the author of the book')
+      }
+    })
 
+    it('should buy the book by index', async () => {
+      await library.buyBook(1, { from: accounts[0], value: 10 * ETHER })
+      const contractBalance = await library.getContractBalance()
+      assert.equal(contractBalance, (10 * ETHER * 5)/100)
+    })
+  })
 })
