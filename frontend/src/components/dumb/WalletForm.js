e91811ce266d83703008f54609b7467298ed5652
patriciarianne <ariannebelits@gmail.com>
2018-09-26 00:46:35 +0800

    Added WalletForm component

--- frontend/src/components/dumb/WalletForm.js
+++ frontend/src/components/dumb/WalletForm.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Row } from 'reactstrap';
 
 const WalletForm = (props) => {
+  const { isGenerated, handleInputChange, saveJsonWallet, generateWallet } = props
+  const downloadJson = (isGenerated) ? (
+    <div>
+      <Label style={{ margin: 15 }}>Save Wallet as JSON file</Label>
+      <Button onClick={saveJsonWallet}>Download</Button>
+    </div>
+  ) : null
   return (
     <Form>
       <FormGroup>
-          <Label>Create Wallet</Label>
-          <Button onClick={props.generateWallet}>Generate Wallet</Button>
-          <Label>Encrypt Wallet</Label>
-          <Button onClick={props.encryptWallet}>Encrypt Wallet</Button>
-          <Label>Save Wallet as JSON</Label>
-          <Button onClick={props.saveJsonWallet}>Save Wallet</Button>
-          
-        </FormGroup>
+        <Row>
+          <Label style={{ fontSize: 40, textAlign: 'center', margin: 10}}>Create New Wallet</Label>
+        </Row>
+        <Row>
+          <Label style={{ fontSize: 20}}>Password</Label>
+          <Input type="password" name="password" onChange={(text) => handleInputChange(text)}/>
+        </Row>
+        <Row style={{ marginTop: 20}}>
+          <Button onClick={generateWallet}>Generate Wallet</Button>
+        </Row>
+        <Row style={{ marginTop: 20}}>
+          {downloadJson}
+        </Row>
+      </FormGroup>
     </Form>
   )
 }
