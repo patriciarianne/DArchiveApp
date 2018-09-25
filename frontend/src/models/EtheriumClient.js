694755ce52689103db1fd598cf7bf2f0c6ddec64
patriciarianne <ariannebelits@gmail.com>
2018-09-25 20:19:25 +0800

    Added EtheriumClient class

---
+++ frontend/src/models/EtheriumClient.js
@@ -0,0 +1,29 @@
+import { provider, ethers } from '../helpers/ethersConfig'
+import Wallet from './Wallet'
+
+class EtheriumClient {
+  constructor() {
+    this.wallet = null
+  }
+
+  static async encryptWallet(wallet, password) {
+    return await wallet.encrypt(password)
+  }
+
+  static async decryptWallet(jsonWallet, password) {
+    return await ethers.Wallet.fromEncryptedWallet(jsonWallet, password)
+  }
+
+  async generateWallet() {
+    this.wallet = new Wallet().walletData
+  }
+
+  
+  static async getBalance(address) {
+    const initialBal = await provider.getBalance(address)
+    let etherBalance = ethers.utils.formatEther(initialBal);
+    return etherBalance
+  }
+}
+
+export default EtheriumClient
