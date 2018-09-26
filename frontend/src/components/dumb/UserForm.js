eab3e16a2267dd73205e02342553de3a28b5cdd9
patriciarianne <ariannebelits@gmail.com>
2018-09-26 21:43:44 +0800

    Modified UserForm component

--- frontend/src/components/dumb/UserForm.js
+++ frontend/src/components/dumb/UserForm.js
@@ -2,16 +2,36 @@ import React from 'react';
 import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 const UserForm = (props) => {
+  const { handleInputChange, isRegistration, values } = props
   return (
     <Form>
+      {
+        isRegistration ? (
+          <FormGroup>
+          <Label>Name</Label>
+          <Input type="text" name="name" id="name" value={values.name || ''} onChange={handleInputChange} />
+        </FormGroup>
+        ) : null
+      }
+      
       <FormGroup>
-          <Label>Email</Label>
-          <Input type="email" name="email" />
+        <Label>Email</Label>
+        <Input type="email" name="email" id="email" value={values.email || ''} onChange={handleInputChange} />
+      </FormGroup>
+      <FormGroup>
+        <Label>Password</Label>
+        <Input type="password" name="password" id="password" value={values.password || ''}onChange={handleInputChange}/>
+      </FormGroup>
+
+      {
+        isRegistration ? (
+          <FormGroup>
+          <Label>Confirm Password</Label>
+          <Input type="password" name="confirmPassword" id="confirmPassword" value={values.confirmPassword || ''} onChange={handleInputChange}/>
         </FormGroup>
-        <FormGroup>
-          <Label>Password</Label>
-          <Input type="password" name="password"/>
-        </FormGroup>
+        ) : null
+      }
+      
     </Form>
   )
 }
