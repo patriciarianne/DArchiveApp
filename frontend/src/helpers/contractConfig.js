44908c7479fa9a25e8dc6ad4560d5e7acd048bca
patriciarianne <ariannebelits@gmail.com>
2018-09-27 20:16:03 +0800

    Modified contract config

--- frontend/src/helpers/contractConfig.js
+++ frontend/src/helpers/contractConfig.js
@@ -2,38 +2,51 @@ import IPFS from 'ipfs-api'
 
 const abi = [
 	{
-		"constant": false,
+		"anonymous": false,
 		"inputs": [
 			{
-				"name": "_title",
+				"indexed": false,
+				"name": "title",
 				"type": "string"
 			},
 			{
-				"name": "_description",
+				"indexed": false,
+				"name": "description",
 				"type": "string"
 			},
 			{
-				"name": "_genre",
+				"indexed": false,
+				"name": "author",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "genre",
 				"type": "string"
 			},
 			{
-				"name": "_price",
+				"indexed": false,
+				"name": "price",
 				"type": "uint256"
 			},
 			{
-				"name": "_linkHash",
+				"indexed": false,
+				"name": "linkHash",
 				"type": "string"
 			},
 			{
-				"name": "_imageHash",
+				"indexed": false,
+				"name": "imageHash",
 				"type": "string"
+			},
+			{
+				"indexed": false,
+				"name": "index",
+				"type": "uint256"
 			}
 		],
-		"name": "addBook",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
+		"name": "BookRemoved",
+		"type": "event"
 	},
 	{
 		"anonymous": false,
@@ -83,51 +96,38 @@ const abi = [
 		"type": "event"
 	},
 	{
-		"anonymous": false,
+		"constant": false,
 		"inputs": [
 			{
-				"indexed": false,
-				"name": "title",
+				"name": "_title",
 				"type": "string"
 			},
 			{
-				"indexed": false,
-				"name": "description",
+				"name": "_description",
 				"type": "string"
 			},
 			{
-				"indexed": false,
-				"name": "author",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"name": "genre",
+				"name": "_genre",
 				"type": "string"
 			},
 			{
-				"indexed": false,
-				"name": "price",
+				"name": "_price",
 				"type": "uint256"
 			},
 			{
-				"indexed": false,
-				"name": "linkHash",
+				"name": "_linkHash",
 				"type": "string"
 			},
 			{
-				"indexed": false,
-				"name": "imageHash",
+				"name": "_imageHash",
 				"type": "string"
-			},
-			{
-				"indexed": false,
-				"name": "index",
-				"type": "uint256"
 			}
 		],
-		"name": "BookRemoved",
-		"type": "event"
+		"name": "addBook",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"constant": false,
@@ -250,10 +250,29 @@ const abi = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "_index",
+				"type": "uint256"
+			}
+		],
+		"name": "getIsBookAuthor",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
 	}
 ]
 
-const address = '0x62063dbfd684deb960276b7a7c07c8513705e087'
+const address = '0xb2f6c1b0c3ed5ec2710989d4fa376112d1609180'
 
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https'})
 
