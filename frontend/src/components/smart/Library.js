3a31c56fd7448f53b0461cb41e152cf17c16a0c8
patriciarianne <ariannebelits@gmail.com>
2018-09-28 01:36:36 +0800

    Modified Library component

--- frontend/src/components/smart/Library.js
+++ frontend/src/components/smart/Library.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react'
 import BookList from '../dumb/BookList'
+import { Button } from 'reactstrap'
+import ethers from 'ethers'
+import { getBookCount, getBookAt, getIsBookAuthor } from '../../helpers/libraryContract'
 
 class Library extends Component {
   constructor(props) {
     super(props)
-    
+    this.state = {
+      books: []
+    }
+
+    this.redirectToFile = this.redirectToFile.bind(this)
   }
 
+  async componentDidMount() {
+    let books = []
+    const bookCount = await getBookCount()
+    for (let index = 0; index < bookCount; index++) {
+      let book = await getBookAt(index)
+      // const isBookAuthor = await getIsBookAuthor(index)
+      const price = ethers.utils.formatEther(book.price)
+      const newBook = {...book, price}
+      books.push(newBook)
+    }
+    this.setState({ books })
+  }
+
+  redirectToFile(fileHash) {
+   console.log(`https://gateway.ipfs.io/ipfs/${fileHash}`)
+  }
+
+  // async isBookAuthor(index) {
+  //   return await getIsBookAuthor(index)
+  // }
+
   render() {
-    const books = [
-      {title: 'title', price: 10},
-      {title: 'title2', price: 2},
-      {title: 'title3', price: 22},
-      {title: 'title4', price: 5},
-      {title: 'title5', price: 23}
-    ]
     return (
       <div>
-        <BookList books={books} />
+        <BookList books={this.state.books} redirectToFile={this.redirectToFile}/>
       </div>
     )
   }
