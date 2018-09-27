5274dece48748317b6b206c40211abb86a09bb1d
patriciarianne <ariannebelits@gmail.com>
2018-09-28 01:36:19 +0800

    Modified WalletView component

--- frontend/src/components/dumb/WalletView.js
+++ frontend/src/components/dumb/WalletView.js
@@ -3,31 +3,6 @@ import { Jumbotron, Label, Row, Button } from 'reactstrap'
 
 const WalletView = (props) => {
   const { wallet } = props
-  const style = {
-    button: {
-      marginRight: '20%',
-      marginLeft: '20%',
-      width: '60%',
-      backgroundColor: '#851E00',
-      color: '#ffff'
-      },
-    label: {
-      fontSize: 28,
-      textAlign: 'center',
-      color: '#851E00',
-    },
-    balance: {
-      fontSize: 50,
-      textAlign: 'center',
-      color: 'black',
-      fontStyle: 'bold',
-    },
-    background: {
-      backgroundColor: '#FEF3D1'
-    }
-
-  }
-  
   return (
     <Jumbotron style={style.background}>
       <Row>
@@ -41,6 +16,30 @@ const WalletView = (props) => {
       </Row>
     </Jumbotron>
   )
+}
+
+const style = {
+  button: {
+    marginRight: '20%',
+    marginLeft: '20%',
+    width: '60%',
+    backgroundColor: '#851E00',
+    color: '#ffff'
+    },
+  label: {
+    fontSize: 28,
+    textAlign: 'center',
+    color: '#851E00',
+  },
+  balance: {
+    fontSize: 50,
+    textAlign: 'center',
+    color: 'black',
+    fontStyle: 'bold',
+  },
+  background: {
+    backgroundColor: '#FEF3D1'
+  }
 }
 
 export default WalletView
