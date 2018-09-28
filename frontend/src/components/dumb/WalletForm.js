8921381f2754cd1c0996f573e5ea39b305565dce
patriciarianne <ariannebelits@gmail.com>
2018-09-28 19:24:53 +0800

    Modified Wallet component

--- frontend/src/components/dumb/WalletForm.js
+++ frontend/src/components/dumb/WalletForm.js
@@ -13,16 +13,16 @@ const WalletForm = (props) => {
     <Form>
       <FormGroup>
         <Row>
-          <Label style={{ fontSize: 40, textAlign: 'center', margin: 10}}>Create New Wallet</Label>
+          <Label style={{ fontSize: 32, textAlign: 'center', margin: 10}}>Create New Wallet</Label>
         </Row>
         <Row>
-          <Label style={{ fontSize: 20}}>Password</Label>
-          <Input type="password" name="password" onChange={(text) => handleInputChange(text)}/>
+          <Label style={{ fontSize: 16}}>Password</Label>
+          <Input type="password" name="walletCreatePassword" id="walletCreatePassword" onChange={(text) => handleInputChange(text)}/>
         </Row>
-        <Row style={{ marginTop: 20}}>
+        <Row style={{ marginTop: 16}}>
           <Button onClick={generateWallet}>Generate Wallet</Button>
         </Row>
-        <Row style={{ marginTop: 20}}>
+        <Row style={{ marginTop: 16}}>
           {downloadJson}
         </Row>
       </FormGroup>
--- frontend/src/components/dumb/WalletView.js
+++ frontend/src/components/dumb/WalletView.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { Jumbotron, Label, Row, Button } from 'reactstrap'
 
 const WalletView = (props) => {
-  const { pendingBalance, confirmedBalance } = props
+  const { pendingBalance, confirmedBalance, withdrawBalance, address } = props
   return (
     <Jumbotron style={style.background}>
+      <Row style={{marginTop: 20}}>
+        <Label style={style.label}>Address:</Label>
+      </Row>
+      <Row>
+        <Label style={style.labelAddress}>{address}</Label>
+      </Row>
       <Row>
         <Label style={style.label}>Pending Balance:</Label>
       </Row>
       <Row>
-        <Label style={style.balance}>{pendingBalance.toFixed(2)} ETH</Label>
-      </Row>
-      <Row>
-        <Button style={style.button}>Withdraw</Button>
+        <Label style={style.balance}>{pendingBalance} ETH</Label>
+        <Button style={style.button} onClick={withdrawBalance}>Withdraw</Button>
       </Row>
       <Row style={{marginTop: 20}}>
         <Label style={style.label}>Confirmed Balance:</Label>
       </Row>
       <Row>
-        <Label style={style.balance}>{confirmedBalance.toFixed(2)} ETH</Label>
+        <Label style={style.balance}>{confirmedBalance} ETH</Label>
       </Row>
     </Jumbotron>
   )
@@ -28,7 +32,8 @@ const style = {
   button: {
     marginRight: '20%',
     marginLeft: '20%',
-    width: '60%',
+    width: '40%',
+    height: '70%',
     backgroundColor: '#851E00',
     color: '#ffff'
     },
@@ -36,6 +41,11 @@ const style = {
     fontSize: 24,
     textAlign: 'center',
     color: '#851E00',
+  },
+  labelAddress: {
+    fontSize: 26,
+    textAlign: 'center',
+    color: '#0000',
   },
   balance: {
     fontSize: 42,
